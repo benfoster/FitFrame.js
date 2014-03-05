9e418b6d8393271a1d2d9f0de19e63b1dc6bec60
Ben Foster <ben@getfabrik.com>
2014-03-05 15:24:13 +0000

    Plugin first draft, demos and docs.

--- README.md
+++ README.md
@@ -2,3 +2,97 @@ FitFrame.js
 ===========
 
 A lightweight, easy-to-use jQuery plugin for responsive iframes.
+
+
+## Setup
+
+FitFrame.js requires jQuery. It has only been tested with jQuery 1.11 so if you have problems with any other versions let me know.
+
+If you're using `mode: wrap` (the default) you'll want to reference `fitframe.css`:
+
+	<link rel="stylesheet" href="fitframe.css" />
+	
+And of course you'll need to reference jQuery and FitFrame.js:
+
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+	<script src="jquery.fitframe.js"></script>
+	
+## Usage (wrap mode)
+
+The default mode of FitFrame.js is `wrap` and uses [intrinsic ratios](http://alistapart.com/article/creating-intrinsic-ratios-for-video) to make the iframes responsive.
+
+By wrapping the `iframe` element in a `div` with `height: 0` and `padding: X` (where X is the ratio between the iframe height and width) we can create a responsive iframe that maintains aspect ratio as its container resizes. So for a 16:9 video the container elements gets a height of 56.25% of it's width `((9/16) * 100)`. Clever stuff I know!
+
+This is the recommended mode since no JavaScript is actually executed when the viewport size changes.
+
+To initialize FitFrame.js just call `fitFrame()` on your container element:
+
+	$('.container').fitFrame();
+
+### Adding elements at runtime
+
+If you're adding DOM elements to your container at runtime you have two options.
+
+1. Just call the `update` function to have FitFrame.js search for any new iframes that have been added and make them responsive:
+
+		$('.container').fitFrame('update');
+	
+2. If you want to wrap the iframe before you add it to the DOM you can use the `wrap` function, passing the iframe object to wrap:
+
+		var wrapped = $('.container').fitFrame('wrap', $(iframeHtml));      
+		$('.add-embed').before(wrapped);
+
+
+You can see an example of this in `demos/demo-wrap.html` that uses [embed.ly](http://embed.ly/) to dynamically load videos using a URL. Note that you'll need to set your embedly API key in the code.
+
+## Usage (resize mode)       
+
+The `resize` mode binds to the resize event of `window` and sets the size of the iframe element to fit the container. No wrapper element is used and you don't need to add any additional CSS.
+
+If you just want to fit your iframes to the width of the container then you are best off using the `wrap` mode. That said, with the use of CSS transitions we can create a more interesting experience when resizing the browser. You can see an example of this in `demos/demo-resize-width.html`. To use `resize` mode:
+
+	$('.container').fitFrame({ mode: 'resize' });
+
+### Adding elements at runtime
+
+Resize mode also supports adding elements at runtime and again you can do these for all new iframes or individually:
+
+1. Resize all new iframes
+
+		$('.container').fitFrame('update');
+	
+2. Resize an individual element *before* adding it to the DOM:
+
+        var resized = $('.container').fitFrame('resize', $(iframeHtml));      
+        $('.add-embed').before(resized);
+        
+
+### Fit iframe to container height
+
+This was the original reason for creating the plugin since I needed to make an iframe always fit the height of its container (without any awful black bars!).
+
+To do this, set the `fitHeight` option to true:
+
+	$('.container').fitFrame({
+	 mode: 'resize',
+	 fitHeight: true
+	});
+	
+You can see an example of this in /demos/demo-resize-height.html`.
+
+## Options
+
+	defaults = {
+	  wrapperCssClass: 'fitframe-wrap',
+	  iframeCssClass: 'fitframe',
+	  mode: 'wrap',
+	  fitHeight: false
+	};
+
+## Cleanup
+
+Like all good citizens, FitFrame.js will cleanup after itself:
+
+	$('.container').fitFrame('destroy');
+	
+And can be safely re-initialized if required.
---
+++ jquery.fitframe.js
@@ -0,0 +1,215 @@
+/******************************************
+ * jQuery FitFrame
+ *
+ * A plugin that makes content collapsible.
+ *
+ * Author          @benfosterdev (http://ben.onfabrik.com)
+ * Copyright       Copyright (c) 2013 Ben Foster.
+ * License         MIT
+ * Github          https://github.com/benfoster/FitFrame.js
+ * Version         0.0.0.1
+ *
+ ******************************************/
+
+;
+(function ($, window, document, undefined) {
+
+  var PLUGIN_NAME = "fitFrame",
+    INSTANCE_KEY = "plugin_" + PLUGIN_NAME;
+
+  function FitFrame(element, options, defaults) {
+    this.element = $(element);
+    this.options = $.extend({}, defaults, options);
+
+    this._defaults = defaults;
+    this._init();
+  }
+
+  FitFrame.prototype = {
+
+    // Fits any new iframes that have been added to the DOM since initialization
+    update: function () {
+      if (this.options.mode === 'resize') {
+        this._resizeNew();
+      } else {
+        this._wrapNew();
+      }
+    },
+
+    // Wraps a single iframe element (mode = 'wrap')
+    wrap: function (iframe) {
+
+      var padding = (this._calculateRatio(iframe) * 100) + '%',
+        wrapper = $('<div/>')
+          .addClass(this.options.wrapperCssClass)
+          .css('padding-bottom', padding);
+
+      // wrap the iframe and return the wrapper
+      return iframe.wrap(wrapper).parent();
+    },
+
+    // Initializes and resizes a single iframe element (mode = 'resize')
+    resize: function (iframe) {
+
+      this._setupIframe(iframe);
+
+      this._resizeIframe(
+        iframe, this.element.width(), this.element.height(), this.options.fitHeight);
+
+      // return the resized iframe
+      return iframe;
+    },
+
+    // Removes and cleans up fitFrame.js
+    destroy: function () {
+      var self = this;
+      if (self.options.mode === 'resize') {
+        // remove iframe css classes
+        this.element.find('iframe.' + this.options.iframeCssClass).each(function () {
+          var data = $(this).data(PLUGIN_NAME);
+          $(this).removeClass(self.options.iframeCssClass)
+            .width(data.width)
+            .height(data.height)
+            .data(PLUGIN_NAME, null);
+        });
+
+        this._unbind();
+
+      } else {
+
+        // remove wrappers
+        this.element.find('.' + this.options.wrapperCssClass).children().unwrap();
+      }
+
+      this.element.data(INSTANCE_KEY, null);
+    },
+
+    _init: function () {
+      if (this.options.mode === 'resize') {
+        this._bind(); // binds to window resize
+      }
+
+      this.update();
+    },
+
+    _wrapNew: function () {
+      var self = this,
+        // select iframes that haven't already been wrapped
+        iframes = this.element.find('iframe')
+          .filter(function () {
+            return !$(this).closest(self.options.wrapperCssClass).length
+          });
+
+      iframes.each(function () {
+        self.wrap($(this));
+      });
+    },
+
+    _resizeNew: function () {
+      var self = this;
+      $('iframe').not(this.options.iframeCssClass).each(function () {
+        self._setupIframe($(this));
+      });
+      this._resizeAll();
+    },
+
+    _setupIframe: function (iframe) {
+      iframe
+        .data(PLUGIN_NAME, {
+          ratio: this._calculateRatio(iframe),
+          width: iframe.width(),
+          height: iframe.height()
+        })
+        .addClass(this.options.iframeCssClass);
+    },
+
+    // resizes all iframes that have been initialized by fitFrame.js
+    _resizeAll: function () {
+
+      var self = this,
+        containerWidth = this.element.width(),
+        containerHeight = this.element.height(),
+        fitHeight = this.options.fitHeight;
+
+      this.element.find('iframe.' + this.options.iframeCssClass).each(function () {
+        self._resizeIframe($(this), containerWidth, containerHeight, fitHeight);
+      });
+    },
+
+    _resizeIframe: function (iframe, containerWidth, containerHeight, fitHeight) {
+      var ratio = iframe.data(PLUGIN_NAME).ratio,
+        newHeight = containerWidth * ratio;
+
+      if (fitHeight && (newHeight > containerHeight)) {
+        // the new height would overlap the container height so scale the width instead
+        iframe.height(containerHeight).width(containerHeight / ratio);
+      } else {
+        // scale the height (essentially what 'wrap' mode is doing)
+        iframe.height(containerWidth * ratio).width('100%');
+      }
+    },
+
+    _calculateRatio: function (iframe) {
+      var width = iframe.attr('width'),
+        height = iframe.attr('height');
+
+      return ((height / width)).toPrecision(4);
+    },
+
+    _bind: function () {
+      // debounced resizing
+      var self = this,
+        t;
+      $(window).on('resize.' + PLUGIN_NAME, function () {
+        clearTimeout(t);
+        t = setTimeout(function () {
+          self._resizeAll();
+        }, 100);
+      });
+    },
+
+    _unbind: function () {
+      // remove all namespaced event handlers
+      $(window).off('.' + PLUGIN_NAME);
+    }
+  };
+
+  $.fn[PLUGIN_NAME] = function (options) {
+
+    var args = arguments,
+      returns;
+
+    this.each(function () {
+
+      var instance = $.data(this, INSTANCE_KEY);
+
+      if (instance) {
+        // check if invoking public methods
+        if (typeof (options) === 'string' && options[0] !== '_') {
+          var method = instance[options];
+          if (typeof (method) === 'function') {
+            returns = method.apply(instance, Array.prototype.slice.call(args, 1));
+          } else {
+            // method missing
+            $.error('Public method \'' + options + '\' does not exist on jQuery.' + PLUGIN_NAME);
+          }
+        }
+
+      } else {
+        $.data(this, INSTANCE_KEY, new FitFrame(this, options, $.fn[PLUGIN_NAME].defaults));
+      }
+    });
+
+    // if the earlier cached method has a value return it, otherwise return this to preserve chainability
+    return returns !== undefined ? returns : this;
+  }
+
+  $.fn[PLUGIN_NAME].defaults = {
+    wrapperCssClass: 'fitframe-wrap',
+    iframeCssClass: 'fitframe',
+    mode: 'wrap',
+    fitHeight: false
+  };
+
+
+})(jQuery, window, document);
